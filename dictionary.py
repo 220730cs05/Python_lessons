e1b2e1777ba31e7be53df1d0f6239af8cbad72ba
Elbek Meliboev <220730@akfauniversity.org>
2022-10-28 00:23:58 +0500

    added new file

---
+++ dictionary.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Elbek Meliboev on Thu Oct 27 23:25:37 2022
+
+@author: VICTUS
+"""
+
+# # .items()
+# student_0 = {
+#     'name': "james",
+#     'surname': "johnson",
+#     'age': 20,
+#     'faculty': "computer science",
+#     'course': 2 
+#     }
+# print(student_0.items())
+# for key, value in student_0.items():
+#     print(f'Key: {key}')
+#     print(f'Value: {value} \n')
+
+
+# mobile_phones = {
+#     'Dave': 'iphone 12',
+#     'Harry': 'galaxy note 9',
+#     'Steve': 'redmi 4',
+#     'Michael': 'nokia 1110',
+#     }
+# for k,v in mobile_phones.items():
+#     print(f'{k.title()}\'s mobile phone is {v}')
+
+# # .keys()
+# products = {
+#     'apple': '1$',
+#     'grape': '2$',
+#     'peach': '3.12$',
+#     'pear': '2.5$'
+#     }
+# print(products.keys())
+
+# print('Grocery products: ')
+# for fruit in products.keys():
+#     print(fruit.title())
+
+
+# shopping = ['bread','grape','sugar','apple','fish']
+# for product in products:
+#     if product in shopping:
+#         print(f'{product.title()} {products[product]}')
+# for thing in shopping:
+#     if thing not in products:
+#         print(f'Please, bring some {thing} to your shop')
