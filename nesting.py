b91fc5f9d1cd0c85fc6c140fd181aabea2c69ec8
Elbek Meliboev <220730@akfauniversity.org>
2022-10-28 00:53:18 +0500

    added new file

---
+++ nesting.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Elbek Meliboev on Fri Oct 28 00:25:24 2022
+
+@author: VICTUS
+"""
+# car0 = {
+#     'model':'Lacetti',
+#     'color':'white',
+#     'year':2018,
+#     'cost':13_000,
+#     'km:':50_000,
+#     'type':'automatic'
+#     }
+# car1 = {
+#         'model':'nexia 3',
+#         'color':'black',
+#         'year':2020,
+#         'cost':9_000,
+#         'km:':23_000,
+#         'type':'mechanic'
+#     }
+# car2 = {
+#        'model':'tracker',
+#        'color':'red',
+#        'year':2019,
+#        'cost':15_000,
+#        'km:':20_000,
+#        'type':'automatic'
+#     }
+
+# =============================================================================
+# car = car0
+# print(f"{car['model'].title()},",
+#       f"{car['color']} color",
+#       f"{car['year']} year, {car['cost']}$")
+# car = car1
+# print(f"{car['model'].title()},",
+#       f"{car['color']} color",
+#       f"{car['year']} year, {car['cost']}$")
+# car = car2
+# print(f"{car['model'].title()},",
+#       f"{car['color']} color",
+#       f"{car['year']} year, {car['cost']}$")
+# This way takes too long
+# =============================================================================
+
+# cars = [car0, car1, car2]
+# for car in cars:
+#     print(f"{car['model'].title()},",
+#           f"{car['color']} color",
+#           f"{car['year']} year, {car['cost']}$")
