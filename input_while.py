8256a9c08a145fabb3ee88baf3fa5f452d1db997
Elbek Meliboev <220730@akfauniversity.org>
2022-10-29 00:49:27 +0500

    added new file

---
+++ input_while.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 28 23:29:33 2022
+
+@author: VICTUS
+"""
+# =============================================================================
+# #input()
+# name = input('What is your name?\n>>>')
+# question = (f'Hello {name.title()}, how old are you?\n')
+# age = int(input(question))
+# height = input('What is your height?\n')
+# height = float(height) 
+# =============================================================================
+
+#  #while()
+# number = 1
+# while number<=5:
+#      print(number, end=' ')
+#      number += 1
+# print('Program is ended')
+
+
+# print('<Program that returns the square of an input number>')
+# question = 'Enter any number '
+# question += "or 'exit' to stop the program\n>>>"
+# value = ' '
+# while value != 'exit':
+#     value = input(question)
+#     if value != 'exit':
+#         print(float(value)**2)
+# print('Program is ended!')
+
+
+#flag
+# print('<Program that returns the square of an input number>')
+# question = 'Enter any number '
+# question += "or 'exit' to stop the program\n>>>"
+# flag = True
+# value = ' '
+# while flag:
+#     value = input(question)
+#     if value == 'exit':
+#         flag = False
+#     else:
+#         print(float(value)**2)
+# print('Program is ended!')
+
+
+# #break
+# print('<Program that returns the square of an input number>')
+# question = 'Enter any number '
+# question += "or 'exit' to stop the program\n>>>"
+# value = ''
+# while True:
+#     value = input(question)
+#     if value == 'exit':
+#           break
+#     else:
+#         print(float(value)**2)
+# print('Program is ended!')
+
+
+# #break for
+# numbers = list(range(1,11)) 
+# for number in numbers:
+#     if number == 5:
+#         break
+#     else:
+#         print(f'The square of {number} is equal to {(number)**2}')
+# print('Program is ended!')
+
+
+# #continue
+# numbers = list(range(1,11)) 
+# for number in numbers:
+#     if number == 5:
+#         continue
+#     else:
+#         print(f'The square of {number} is equal to {(number)**2}')
+# print('Program is ended!')
+
+
+# #continue while
+# number = 0
+# while number<10:
+#     number += 1
+#     if number%2 != 0:
+#         continue
+#     else:
+#         print(number)
+
+
+# =============================================================================
+# #infinite loop
+# number = 0
+# while number<10:
+#     if number%2 != 0:
+#         continue
+#     else:
+#         print(number)
+# 
+# number = 1
+# while number>0:
+#     number += 1
+#     if number%2 != 0:
+#         continue
+#     else:
+#         print(number)
+# =============================================================================
