119a51b668839c9d41cda78dd1a679340ce93ad1
Elbek Meliboev <220730@akfauniversity.org>
2022-10-29 02:52:21 +0500

    added new file

---
+++ functions.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 01:57:57 2022
+
+@author: VICTUS
+"""
+
+# =============================================================================
+# def say_hello(name):
+#         """After entering user's name, say him Hello"""
+#         print(f'Hello, MR.{name.title()}!')
+#     
+# say_hello(input('Enter your name: '))
+# =============================================================================
+
+
+# =============================================================================
+#                        #REMEMBER
+# # VALUE WHICH IS GIVEN BY USER IS CALLED ARGUMENT (FOR USERS) 
+# # RECIEVED VALUES FROM USER IS CALLED PARAMETERS (FOR DEVELOPERS)
+# # THEY ARE SIMILAR
+# 
+# #DOCSTRING - INFORMATION ABOUT THE FUNCTION
+# =============================================================================
+
+
+# =============================================================================
+# def calculate_age(name, born_year):
+#     """Function that calculates user's age by year of born"""
+#     print(f'{name.title()} is {2022-born_year} years old')
+# calculate_age(input('Enter your name: '), int(input('When were you born(enter year): ')))
+# =============================================================================
+
+
+# =============================================================================
+# def squareof_num(number):
+#     """Function that calculates square of entered number"""
+#     print(f'The suqare of {number} is {number**2}')
+# def cubeof_num(number):
+#     print(f"The cube of {number} is {number**3}")
+# number = int(input('Enter the number: '))
+# squareof_num(number)
+# cubeof_num(number)
+# =============================================================================
+
+
+# =============================================================================
+# def even_odd(number):
+#     """Function that determines the number even or odd"""
+#     if number%2==0:
+#         print(f'{number} is even')
+#     else:
+#         print(f'{number} is odd')
+# even_odd(int(input('Enter integer number: ')))
+# =============================================================================
+
+
+# =============================================================================
+# def greater(num1,num2):
+#     if num1>num2:
+#         print(f'Greater number is {num1}')
+#     elif num1==num2:
+#         print('They are equal')
+#     else:
+#         print(f'Greater number is {num2}')
+# num1 = int(input("First number: "))
+# num2 = int(input("Second number: "))
+# greater(num1,num2)
+# =============================================================================
