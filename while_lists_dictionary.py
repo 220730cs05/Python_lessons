a829efec7d6c1f1806c0e68c56886c7597a6c689
Elbek Meliboev <220730@akfauniversity.org>
2022-10-29 01:43:23 +0500

    added new file

---
+++ while_lists_dictionary.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 00:50:59 2022
+
+@author: VICTUS
+"""
+
+# =============================================================================
+# print('Make a list of your best friends')
+# names = []
+# n = 1
+# while True:
+#     question = f"Enter your {n} friend's name: "
+#     name = input(question)
+#     names.append(name)
+#     repeat = input('do you add name again? (yes/no): ')
+#     n += 1
+#     if repeat != 'yes':
+#         break
+# print('List of your best friends: ')
+# for name in names:
+#     print(name.title())
+# =============================================================================
+
+# =============================================================================
+# # removing only all selected elements from list 
+# cars = ['bmw','mercedes','ford','toyota','mustang','genesis','toyota']
+# while 'toyota' in cars: #also possible put variable instead of element
+#     cars.remove('toyota') 
+# print(cars)
+# =============================================================================
+
+
+# =============================================================================
+# students = ['james','nick','alex','thomas','harry']
+# graded_students = {}
+# while students:
+#     student = students.pop()
+#     grade = input(f"Enter {student.title()}'s grade: ")
+#     print(f"{student.title()} is graded")
+#     graded_students[student] = int(grade)
+# print(graded_students)
+# =============================================================================
